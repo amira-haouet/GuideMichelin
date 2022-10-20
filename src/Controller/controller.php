81303ffa031ec7dbf19b76b9f737ba3a5cc1525c
amira-haouet <haouetamira@gmail.com>
2022-10-20 16:26:59 +0200

    add controller

---
+++ src/Controller/controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class MichelinController
+{
+    public function accueil()
+    {
+        return new Response("<html><body>
+        <h1><b>Bonjour: </b><p> Guide Michelin ... trouvez un
+        restaurant</p></h1></body></html>");
+    }
+}
