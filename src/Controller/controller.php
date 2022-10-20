2cae7d6da8dbaff9e9220fd2334ce4a4de4f8522
amira-haouet <haouetamira@gmail.com>
2022-10-20 17:03:39 +0200

    add twig and chang controller

--- src/Controller/controller.php
+++ src/Controller/controller.php
@@ -6,10 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class MichelinController
 {
+    ##  public function accueil()
+    ##  {
+    ##    return new Response("<html><body>
+    ##    <h1><b>Bonjour: </b><p> Guide Michelin ... trouvez un
+    ##     restaurant</p></h1></body></html>");
+    ## }
+
+
     public function accueil()
     {
-        return new Response("<html><body>
-        <h1><b>Bonjour: </b><p> Guide Michelin ... trouvez un
-        restaurant</p></h1></body></html>");
+        return $this->render(
+            'accueil/afficher.html.twig',
+        );
     }
 }
